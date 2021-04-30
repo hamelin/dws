5f994854a5501699e5f17ec22b29277ba847b4fb
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2021-04-30 12:06:33 +0000

    Add script to manage versioning

---
+++ script/set-version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+branch_current="$(git branch | awk '/^\* +/{print $2}')"
+if [ "$branch_current" != "main" ]; then
+    read -ep "Current branch is not main; it is $current_branch. Proceed nonetheless? [yN] "
+    echo "$REPLY" | xargs | grep -qvi '^y' && exit 1
+fi
+if [ -n "$(git status --short)" ]; then
+    echo "Repository is dirty. Git status:" 1>&2
+    echo 1>&2
+    git status | awk '{printf("    %s\n", $0)}' 1>&2
+    echo 1>&2
+    read -ep "Proceed with setting new version nonetheless? [yN] "
+    echo "$REPLY" | xargs | grep -qvi '^y' && exit 1
+fi
+echo -n "Current version:   " 1>&2
+sed -Ene '2{s/VERSION="([^"]*)"/\1/; p}' dws 1>&2
+read -ep "Input new version: "
+version_new="$(echo "$REPLY" | xargs)"
+read -ep "New version will be $version_new. Are you sure? [yN] "
+echo "$REPLY" | xargs | grep -qvi '^y' && exit 1
+
+sed -ie "2s/^.*$/VERSION=\"$version_new\"/" dws || exit
+if ! git tag --sign --message="Release version $version_new" "$version_new" ; then
+    x=$?
+    echo "Problem while adding the version tag, so version change is reverted." 1>&2
+    git checkout dws
+    exit $x
+fi
+if ! git push --tags; then
+    x=$?
+    echo "There was a problem while pushing the tags to the origin remote, you may want to look into that. However, the version change *stays*." 1>&2
+    exit $x
+fi
